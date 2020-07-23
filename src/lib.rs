664737b9eb6ebce4f416cc2490158517f15bf1c5
DarkSky <darksky2048@gmail.com>
2020-07-23 14:36:52 +0800

    add mkdir_after_remove and inline

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::fs::{create_dir_all, metadata};
+use std::fs::{create_dir, create_dir_all, metadata, remove_dir_all, remove_file};
 use std::io::{self, Result};
 use std::path::is_separator;
 use walkdir::WalkDir;
@@ -15,13 +15,16 @@ pub trait PathExt {
     fn is_file(&self) -> bool;
     fn is_dir(&self) -> bool;
     fn walk_dir<F: Fn(&Path) -> bool>(&self, filter: F) -> Vec<PathBuf>;
+    fn mkdir_after_remove(&self) -> io::Result<PathBuf>;
 }
 
 impl<T: AsRef<Path>> PathExt for T {
+    #[inline]
     fn full_str(&self) -> &str {
         self.as_ref().to_str().unwrap_or_default()
     }
 
+    #[inline]
     fn ext_str(&self) -> &str {
         self.as_ref()
             .extension()
@@ -29,6 +32,7 @@ impl<T: AsRef<Path>> PathExt for T {
             .unwrap_or_default()
     }
 
+    #[inline]
     fn stem_str(&self) -> &str {
         self.as_ref()
             .file_stem()
@@ -36,6 +40,7 @@ impl<T: AsRef<Path>> PathExt for T {
             .unwrap_or_default()
     }
 
+    #[inline]
     fn name_str(&self) -> &str {
         self.as_ref()
             .file_name()
@@ -43,6 +48,7 @@ impl<T: AsRef<Path>> PathExt for T {
             .unwrap_or_default()
     }
 
+    #[inline]
     fn create_parent_dir_all(&self) -> io::Result<()> {
         if let Some(root) = self.as_ref().parent() {
             if !root.exists() {
@@ -52,6 +58,20 @@ impl<T: AsRef<Path>> PathExt for T {
         Ok(())
     }
 
+    #[inline]
+    fn mkdir_after_remove(&self) -> io::Result<PathBuf> {
+        if self.as_ref().exists() {
+            if self.as_ref().is_file() {
+                remove_file(self)?;
+            } else {
+                remove_dir_all(self)?;
+            }
+        }
+        create_dir(self)?;
+        Ok(self.as_ref().into())
+    }
+
+    #[inline]
     fn merge<P: AsRef<Path>>(&self, append: P) -> PathBuf {
         self.as_ref()
             .iter()
@@ -71,18 +91,21 @@ impl<T: AsRef<Path>> PathExt for T {
             .collect()
     }
 
+    #[inline]
     fn is_file(&self) -> bool {
         metadata(self)
             .map(|metadata| metadata.is_file())
             .unwrap_or(false)
     }
 
+    #[inline]
     fn is_dir(&self) -> bool {
         metadata(self)
             .map(|metadata| metadata.is_dir())
             .unwrap_or(false)
     }
 
+    #[inline]
     fn walk_dir<F: Fn(&Path) -> bool>(&self, filter: F) -> Vec<PathBuf> {
         WalkDir::new(self)
             .into_iter()
